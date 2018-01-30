d506a4178b4276fe424b40bcfa425d77701b9e63
Robert Sellers <robertwsellers@gmail.com>
2018-01-30 14:50:56 -0500

    R syntax

--- R/zip_validation.R
+++ R/zip_validation.R
@@ -1,5 +1,5 @@
 csv_validation <- function(pointfile, ...){
-  if(substring(tolower(pointfile), nchar(pontfile)-3) != ".zip"){
+  if(substring(tolower(pointfile), nchar(pointfile)-3) != ".zip"){
     stop('Uploaded data needs to be CSV file. ');
   }else{
     fname = pointfile$Name
