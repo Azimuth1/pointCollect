713b0612cf386040dbdacc689ca691e71125d9f3
Robert Sellers <robertwsellers@gmail.com>
2018-01-30 15:00:17 -0500

    push test

--- R/zip_validation.R
+++ R/zip_validation.R
@@ -1,5 +1,6 @@
 csv_validation <- function(pointfile, ...){
   if(substring(tolower(pointfile), nchar(pointfile)-3) != ".csv"){
+    # check if input is .CSV
     stop('Uploaded data needs to be CSV file. ');
   }else{
     fname = pointfile$Name
